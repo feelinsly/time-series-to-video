1e0985bb0197bc0acc9a35e39291bd86a49f1f4c
Xiao Ge <liz.gexiao@gmail.com>
2021-10-26 16:44:30 -0700

    Update run_vocal_pitch.sh

--- run_vocal_pitch.sh
+++ run_vocal_pitch.sh
@@ -1,2 +1,2 @@
-python3 animate_timeseries.py --csv vocal_pitch_example_csv.csv --val_name='Vocal Pitch (standardized)' --val_unit='' --out vocal_pitch_example_video.mp4 --time_col_name=tnstart_absolute --end_time_col_name=tnend_absolute --val_col_name=pmean_rescale --color=brown --fill_na=None --chart=scatter
+python3 animate_timeseries_vocal_pitch.py --csv vocal_pitch_example_csv.csv --val_name='Vocal Pitch (standardized)' --val_unit='' --out vocal_pitch_example_video.mp4 --time_col_name=tnstart_absolute --end_time_col_name=tnend_absolute --val_col_name=pmean_rescale --color=brown --fill_na=None --chart=scatter
 
